8b896740413f1fe56f75670cfc4dbd7e1c8e6c0d
Muhammad Andianto <andianto.muh@gmail.com>
2023-07-29 18:19:56 +0700

    first commit, basic function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){ // Ini syntax buat state
+    super(props)
+    this.state = {
+        number: 0
+    }
+  }
+  onIncr = () => {
+    let plus = this.state.number + 1;
+    this.setState({number: plus})
+  }
+  onDecr = () => {
+    let minus = this.state.number - 1;
+    this.setState({number: minus})
+  }
+  render () {
+    return (
+      <div>
+        <button onClick={this.onDecr}>Decr</button>
+        <p>{this.state.number}</p>
+        <button onClick={this.onIncr}>Incr</button>
+      </div>
+    );
+  }
 }
 
 export default App;
