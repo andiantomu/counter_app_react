d482b8a098b219ae4fdf613466c7fc5b1deae7f1
Muhammad Andianto <andianto.muh@gmail.com>
2023-07-29 20:15:51 +0700

    Using setState

--- src/App.js
+++ src/App.js
@@ -2,26 +2,32 @@ import React from "react";
 import './App.css';
 
 class App extends React.Component {
-  constructor(props){ // Ini syntax buat state
+  // Create state, using local state
+  constructor(props){
     super(props)
     this.state = {
         number: 0
     }
   }
+  // Function to increase the number, using setState
   onIncr = () => {
     let plus = this.state.number + 1;
     this.setState({number: plus})
   }
+  // // Function to decrease the number, using setState
   onDecr = () => {
     let minus = this.state.number - 1;
     this.setState({number: minus})
   }
   render () {
     return (
-      <div>
-        <button onClick={this.onDecr}>Decr</button>
-        <p>{this.state.number}</p>
-        <button onClick={this.onIncr}>Incr</button>
+      <div className="main">
+        <h1>Counter App</h1>
+        <div className="container">
+          <button onClick={this.onDecr}>--</button>
+          <p>{this.state.number}</p>
+          <button onClick={this.onIncr}>++</button>
+        </div>
       </div>
     );
   }
